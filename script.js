01dab2508dce976a71cd99475e55fe94715e5758
adibasultan <theadibasultan@gmail.com>
2021-06-10 16:12:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["FrontEnd Developer","BackEnd Developer","FullStackDeveloper","Gamer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
